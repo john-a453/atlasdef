64a974078aa8e5c4e7fd00bdcf72dfdf53f7148f
john-a453 <128242582+john-a453@users.noreply.github.com>
2025-07-11 12:06:28 +0100

    Enhanced SOC as a Service Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPage';
 import ServiceDetailPage from './pages/ServiceDetailPage';
+import SOCServicePage from './pages/SOCServicePage';
 import IndustriesPage from './pages/IndustriesPage';
 import PartnersPage from './pages/PartnersPage';
 import AboutPage from './pages/AboutPage';
@@ -16,6 +17,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="services" element={<ServicesPage />} />
         <Route path="services/:category/:service" element={<ServiceDetailPage />} />
+        <Route path="soc-service" element={<SOCServicePage />} />
         <Route path="industries" element={<IndustriesPage />} />
         <Route path="partners" element={<PartnersPage />} />
         <Route path="about" element={<AboutPage />} />
